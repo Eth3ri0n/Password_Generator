8dfde9250d25587467af01968393e5051c9e32f5
Eth3ri0n <eth3rias@gmail.com>
2024-03-28 00:12:26 +0100

    Ajoutez des fichiers projet.

---
+++ Password_Generator/Program.cs
@@ -0,0 +1,67 @@
+﻿using Ask_Password_Generator;
+using System;
+
+namespace Password_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NUMBER_OF_PASSWORDS = 10;
+
+            int PasswordLength = tools.AskPositiveNumberNotNull("Enter the length of the password: ");
+
+            Console.WriteLine();
+            int passwordPreference = tools.AskNumberBetween("What kind of password do you want ? : \n" +
+                "1 - Only with lowercase characters \n" +
+                "2 - Only with lowercase and uppercase characters \n" +
+                "3 - Only with characters and numbers \n" +
+                "4 - Characters, numbers and special characters \n" +
+                "You choice : ", 1, 4);
+
+            string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+            string upperCase = lowerCase.ToUpper();
+            string numbers = "0123456789";
+            string specialCharacters = "{~[-|_^!@#$%^&*()_+]=£%*µ/!§+}";
+            string password = "";
+            string alphabet;
+
+            Random random = new Random();
+
+            // Generate the password
+            if (passwordPreference == 1)
+            {
+                alphabet = lowerCase;
+            }
+            else if (passwordPreference == 2)
+            {
+                alphabet = lowerCase + upperCase;
+            }
+            else if (passwordPreference == 3)
+            {
+                alphabet = lowerCase + upperCase + numbers;
+            }
+            else
+            {
+                alphabet = lowerCase + upperCase + numbers + specialCharacters;
+            }
+
+            int alphabetLength = alphabet.Length;
+            int index = random.Next(0, alphabetLength);
+            password += alphabet[index];
+
+            // Display the password
+            for (int i = 0; i < NUMBER_OF_PASSWORDS; i++)
+            {
+                password = "";
+                for (int j = 1; j < PasswordLength; j++)
+                {
+                    index = random.Next(0, alphabetLength);
+                    password += alphabet[index];
+                }
+
+                Console.WriteLine("Generated password: " + password);
+            }
+        }
+    }
+}
---
+++ Password_Generator/tools.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ask_Password_Generator
+{
+    static class tools
+    {
+        // Ask a positive number not null
+        public static int AskPositiveNumberNotNull(string question)
+        {
+            return AskNumberBetween(question, 1, int.MaxValue, "ERROR : The number must be positive and not null");
+        }
+
+        // Ask a number between min and max
+        public static int AskNumberBetween(string question, int min, int max, string customErrorMessage = null)
+        {
+            // Ask a number
+            while (true)
+            {
+                int number = AskNumber(question);
+
+                // Check if the number is between min and max
+                if ((number >= min) && (number <= max))
+                {
+                    return number;
+                }
+
+                // Display an error message
+                if (customErrorMessage != null)
+                {
+                    Console.WriteLine(customErrorMessage);
+                }
+                else
+                {
+                    Console.WriteLine($"ERROR : The number must be between {min} and {max}.");
+
+                }
+
+                Console.WriteLine();
+
+                return AskNumberBetween(question, min, max, customErrorMessage);
+
+            }
+        }
+
+        // Ask a number
+        public static int AskNumber(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                string answer = Console.ReadLine();
+
+                // Check if the answer is a number
+                try
+                {
+                    int answerParse = int.Parse(answer);
+                    return answerParse;
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR : You must enter a number.");
+                    Console.WriteLine();
+                }
+            }
+        }
+
+    }
+}
